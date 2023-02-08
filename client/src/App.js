ec80764b9a6a54c371c2e9643f06d7238f92a010
汪志鹏 <13476859295@163.com>
2023-02-08 14:19:21 +0800

    add the text editor

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
-
+import TextEditor from "./TextEditor"
 function App() {
   return (
-    <div className="App"></div>
+    <TextEditor />
   );
 }
 
---
+++ client/src/TextEditor.js
@@ -0,0 +1,16 @@
+import {useCallback } from 'react';
+import Quill from 'quill';
+import "quill/dist/quill.snow.css";
+
+export default function TextEditor(props) {
+    const wrapperRef = useCallback(wrapper => {
+        if (wrapper == null) return
+        wrapper.innerHTML = ""
+        const editor = document.createElement("div")
+        wrapper.append(editor)
+        new Quill(editor, {theme:"snow"})
+    },[]);
+    return (
+        <div id="container" ref={wrapperRef}></div>
+    );
+}
